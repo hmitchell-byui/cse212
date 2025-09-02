a4ad915d9433aac47cc609066f0256682a1047c6
hmitchell-byui <hm1tch3ll99@gmail.com>
2025-09-02 16:25:06 -0500

    Week1 Assignment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -6,15 +6,27 @@ public static class Arrays
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    /// 
+    /// My first idea is to create a fixed array of the given length.
+    /// Then I will use a for loop to iterate through the array.
+    /// In each iteration, I will calculate the multiple by multiplying the number by (i + 1) where i is the current index.
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        number = Math.Round(number, 2);
+        double[] multiples = new double[length];    
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = Math.Round(number * (i + 1), 2);
+        }
+        return multiples
 
-        return []; // replace this return statement with your own
+        ; // replace this return statement with your own
     }
+
 
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
@@ -23,11 +35,24 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
+    /// 
+    /// My first idea is to use a loop to perform the rotation 'amount' times.
+    /// In each iteration, I will remove the first element of the list and add it to the end of the list.
+    /// This will effectively rotate the list to the right by one position in each iteration.
+    /// After completing the loop for the specified 'amount', the list will be rotated as required.
+    /// Amount will be used as one less than the actual amount to account for zero indexing.
+    /// </summary>
+    /// <param name="data">the list to be rotated</param>
+    /// <param name="amount">the amount to rotate the list</param>
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        List<int> tail = data.GetRange(data.Count - amount, amount); // Get the last 'amount' elements
+        data.RemoveRange(data.Count - amount, amount); // Remove the last 'amount' elements from the original list
+        data.InsertRange(0, tail); // Insert the removed elements at the beginning of the list
+
     }
 }
